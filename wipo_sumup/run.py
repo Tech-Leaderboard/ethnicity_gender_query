82ca7de029a4aa7e3fe75dd854bf4a43b06c7f63
TweedleTween <nappingbird0304@gmail.com>
2018-06-14 10:59:25 -0700

    Change to cal the output country is null only when in data source it's null

--- wipo_sumup/run.py
+++ wipo_sumup/run.py
@@ -4,17 +4,17 @@ import sys
 
 print('first_name,country_code,gender,probability,count')
 
-in_files = ['wgnd_ctry.csv', 'wgnd_langctry.csv', 'wgnd_noctry.csv', 'wgnd_source.csv']
-name_idx = [0, 0, 0, 0]
-country_idx = [1, 5, -1, 1]
-gender_idx = [2, 1, 1, 2]
+in_files = ['wgnd_ctry.csv', 'wgnd_langctry.csv', 'wgnd_noctry.csv']
+name_idx = [0, 0, 0]
+country_idx = [1, 5, -1]
+gender_idx = [2, 1, 1]
 writer = csv.writer(sys.stdout)
 # Only counts when country_code is None
 table_name_cnt = {}
 table_gender_cnt = {}
 # Only counts when country_code is not None
-table_name2_cnt = {}
-table_gender2_cnt = {}
+table_name_cnt = {}
+table_gender_cnt = {}
 
 for i, in_file in enumerate(in_files):
     in_file = open(in_file)
@@ -32,27 +32,14 @@ for i, in_file in enumerate(in_files):
 
         for j in space_idx:
             tmp_name = name[:j].lower()
-            table_name_cnt[tmp_name] = table_name_cnt.get(tmp_name, 0) + 1
-            table_gender_cnt[tmp_name] = table_gender_cnt.get(tmp_name, 0) + (1 if row[gender_idx[i]] == 'M' else 0)
-
-            if len(country_code) == 0:
-                continue
             pair = (tmp_name.lower(), country_code.lower())
-            table_name2_cnt[pair] = table_name2_cnt.get(pair,0) + 1
-            table_gender2_cnt[pair] = table_gender2_cnt.get(pair, 0) + (1 if row[gender_idx[i]] == 'M' else 0)
+            table_name_cnt[pair] = table_name_cnt.get(pair,0) + 1
+            table_gender_cnt[pair] = table_gender_cnt.get(pair, 0) + (1 if row[gender_idx[i]] == 'M' else 0)
 
-for name in table_name_cnt:
-    probability = float(table_gender_cnt[name]) / float(table_name_cnt[name])
+for name, country_code in table_name_cnt:
+    probability = float(table_gender_cnt[(name, country_code)]) / float(table_name_cnt[(name, country_code)])
     gender = 'male' if probability >= 0.5 else 'female'
     if probability < 0.5:
         probability = 1 - probability
-    cnt = table_name_cnt[name]
-    writer.writerow([name, '', gender, probability, cnt])
-
-for name, country_code in table_name2_cnt:
-    probability = float(table_gender2_cnt[(name, country_code)]) / float(table_name2_cnt[(name, country_code)])
-    gender = 'male' if probability >= 0.5 else 'female'
-    if probability < 0.5:
-        probability = 1 - probability
-    cnt = table_name2_cnt[(name, country_code)]
+    cnt = table_name_cnt[(name, country_code)]
     writer.writerow([name, country_code, gender, probability, cnt])
